398178cf559096522e2d103931b2f6dde9171ba3
nithindaniel1 <nithindaniel2018@gmail.com>
2022-12-19 18:59:34 +0530

    add product added

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var adminRouter = require('./routes/admin');
 var hbs = require('express-handlebars')
 var app = express();
 
+var fileUpload=require('express-fileupload')
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -19,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(fileUpload())
 
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
