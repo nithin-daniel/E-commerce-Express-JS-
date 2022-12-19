d1dec7ab8aed6f71d479364063b1e155ce44c709
nithindaniel1 <nithindaniel2018@gmail.com>
2022-12-19 19:12:05 +0530

    mongodb config file setup

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var hbs = require('express-handlebars')
 var app = express();
 
 var fileUpload=require('express-fileupload')
+var db=require('./config/connections')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
+db.connect()
 
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
